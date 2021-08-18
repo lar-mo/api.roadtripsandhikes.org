e95c0e1c4b1803ea1c23958c47d5de7740f68d48
Larry Moiola <lmoiola@gmail.com>
2021-08-17 22:08:45 -0700

    first commit

---
+++ rtah_apis/rtah_apis/urls.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from django.urls import path, include
+# from rest_framework import routers
+
+# from pmd_strings import views
+
+# router = routers.DefaultRouter()
+# router.register('strings/v1', views.StringViewSetv1)
+# router.register('strings/v2', views.StringViewSetv2)
+# router.register('strings/v3', views.StringViewSetv3)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    # path('', include(router.urls)),
+    # path('api-auth/', include('rest_framework.urls')),
+    # path('', include('api_wrapper.urls')),
+    path('wrapper/', include('api_wrapper.urls'))
+]
