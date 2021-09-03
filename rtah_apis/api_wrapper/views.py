559f52fd911343f046163363881a17c8668efefe
Larry Moiola <lmoiola@gmail.com>
2021-09-03 10:52:45 -0700

    updating bloggerApiGetLatestPost view

--- rtah_apis/api_wrapper/views.py
+++ rtah_apis/api_wrapper/views.py
@@ -44,3 +44,25 @@ def bloggerApiGetLatestPost(request):
     post_info = {'latest_post': {'published': post_published, 'post_url': post_url, 'title': post_title, 'image_url': post_image_url }}
     post_info_sanitized = json.dumps(post_info, indent = 4)
     return HttpResponse(post_info_sanitized, content_type='application/json')
+
+def bloggerApiGetLatestPost(request):
+    blogger_apiv3 = get_secret('blogger_apiv3')
+    headers = {"Referer": "https://api.roadtripsandhikes.org"}
+    response = requests.get("https://www.googleapis.com/blogger/v3/blogs/5929721860344397604/posts?",
+        params = {
+            'key': blogger_apiv3,
+            'fetchBodies': 'true',
+            'fetchImages': 'true',
+            'maxResults': 1,
+            'orderBy': 'PUBLISHED',
+        },
+        headers=headers
+    )
+    post_info_json = response.json().pop('items')
+    post_published = post_info_json[0]['published']
+    post_url = post_info_json[0]['url']
+    post_title = post_info_json[0]['title']
+    post_image_url = post_info_json[0]['images'][0]['url']
+    post_info = {'latest_post': {'published': post_published, 'post_url': post_url, 'title': post_title, 'image_url': post_image_url }}
+    post_info_sanitized = json.dumps(post_info, indent = 4)
+    return HttpResponse(post_info_sanitized, content_type='application/json')
