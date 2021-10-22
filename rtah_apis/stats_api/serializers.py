9d08009d36e24b0d5dceb53314fb5fec62d6bf9e
Larry Moiola <lmoiola@gmail.com>
2021-10-21 23:11:46 -0700

    updating var name to unique val in overall stats and fields to catchall in hike serializer

--- rtah_apis/stats_api/serializers.py
+++ rtah_apis/stats_api/serializers.py
@@ -5,24 +5,32 @@ from .models import Hike, Person
 
 class PersonSerializer(serializers.ModelSerializer):
     hikes = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
-    
+
     all_hikes = Hike.objects.all()
     def totalhikes(self,obj):
-        # print(obj.id)
         result = self.all_hikes.filter(hiker__id=obj.id).aggregate(Count('id'))
         return result['id__count']
 
     def total_mi(self, obj):
         result = self.all_hikes.filter(hiker__id=obj.id).aggregate(Sum('distance_mi'))
-        return round(result['distance_mi__sum'], 2)
+        try:
+            return round(result['distance_mi__sum'], 2)
+        except:
+            return 0
 
     def total_elev_ft(self, obj):
         result = self.all_hikes.filter(hiker__id=obj.id).aggregate(Sum('elevation_gain_ft'))
-        return result['elevation_gain_ft__sum']
+        res = result['elevation_gain_ft__sum']
+        if not res:
+            return 0
+        return res
 
     def highest_elev_ft(self, obj):
         result = self.all_hikes.filter(hiker__id=obj.id).aggregate(Max('highest_elev_ft'))
-        return result['highest_elev_ft__max']
+        res = result['highest_elev_ft__max']
+        if not res:
+            return 0
+        return res
 
     total_hikes = serializers.SerializerMethodField('totalhikes')
     total_miles = serializers.SerializerMethodField('total_mi')
@@ -33,14 +41,12 @@ class PersonSerializer(serializers.ModelSerializer):
         model = Person
         fields = ('id','first_name','last_name','join_date','email','profile_img','hikes','total_hikes','total_miles','total_elev_feet','highest_elev_feet')
         # fields = '__all__'
-        # read_only_fields = ('join_date',)
 
 class HikeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Hike
-        # fields = '__all__'
-        fields = ('id', 'hike_date', 'location', 'state', 'distance_mi', 'elevation_gain_ft', 'highest_elev_ft', 'alltrails_url', 'blogger_url', 'hiker',)
-        # depth = 1
+        fields = '__all__'
+        # fields = ('id', 'hike_date', 'location', 'state', 'distance_mi', 'elevation_gain_ft', 'highest_elev_ft', 'alltrails_url', 'blogger_url', 'hiker',)
 
     def to_representation(self, instance):
         self.fields['hiker'] =  PersonSerializer(read_only=True)
