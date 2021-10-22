8881742e109edc92936fb15df30f4a25ea67a96d
Larry Moiola <lmoiola@gmail.com>
2021-10-21 22:20:18 -0700

    removing unnecessary imports

--- rtah_apis/stats_api/models.py
+++ rtah_apis/stats_api/models.py
@@ -1,10 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User
-from django.forms import ModelForm
-
 from django.utils import timezone
-import datetime
-import django, platform
 
 class Person(models.Model):
     first_name          = models.CharField(max_length=100, blank=False)
