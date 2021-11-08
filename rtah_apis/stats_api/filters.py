b85828d21d2f228584bf8a6de672ffdb28554f13
Larry Moiola <lmoiola@gmail.com>
2021-11-07 18:30:00 -0800

    updating formating

--- rtah_apis/stats_api/filters.py
+++ rtah_apis/stats_api/filters.py
@@ -2,9 +2,7 @@ import django_filters
 from .models import Hike, Person
 
 class HikerFilter(django_filters.FilterSet):
-    hiker = django_filters.ModelChoiceFilter(field_name="hiker",
-                                             queryset=Person.objects.all())
-
+    hiker = django_filters.ModelChoiceFilter(field_name="hiker", queryset=Person.objects.all())
     start_date = django_filters.DateFilter(field_name='hike_date', lookup_expr='gte')
     end_date = django_filters.DateFilter(field_name='hike_date', lookup_expr='lt')
     year = django_filters.NumberFilter(field_name='hike_date', lookup_expr='year')
