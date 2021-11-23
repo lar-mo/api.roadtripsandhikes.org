bc7717afb3dd657374b06a54ee9089f373891980
Larry Moiola <lmoiola@gmail.com>
2021-11-23 11:11:43 -0800

    adding route for /hiking-stats/for

--- rtah_apis/stats_api/urls.py
+++ rtah_apis/stats_api/urls.py
@@ -6,6 +6,7 @@ app_name = 'stats_api'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('for/', views.index, name='index'),
     path('for/<int:hiker_id>/', views.hiking_stats_for, name='hiking_stats_for_by_id'),
     path('for/<str:hiker_slug>/', views.hiking_stats_for_slug, name='hiking_stats_for_by_slug'),
 ]
