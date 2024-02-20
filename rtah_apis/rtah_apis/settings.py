4b8a1f339069c4c7d6c54ffab4c769e18b787b37
Larry Moiola <lmoiola@gmail.com>
2024-02-20 12:39:37 -0800

    syncing config changes made in production

--- rtah_apis/rtah_apis/settings.py
+++ rtah_apis/rtah_apis/settings.py
@@ -1,7 +1,10 @@
 import json
 import os
+import pymysql
 from pathlib import Path
 from django.core.exceptions import ImproperlyConfigured
+
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,7 +27,7 @@ def get_secret(setting, secrets=secrets):
 SECRET_KEY = get_secret('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1','.roadtripsandhikes.org']
 
@@ -32,6 +35,7 @@ CORS_ORIGIN_ALLOW_ALL = False
 CORS_ORIGIN_WHITELIST = (
        'http://localhost',
        'https://www.roadtripsandhikes.org',
+       'https://test.roadtripsandhikes.org',
 )
 
 # Application definition
@@ -44,6 +48,7 @@ INSTALLED_APPS = [
     'django_filters',
     'mathfilters',
     'corsheaders',
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -61,6 +66,7 @@ REST_FRAMEWORK = {
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -95,27 +101,26 @@ WSGI_APPLICATION = 'rtah_apis.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'OPTIONS': {
-#           'sql_mode': 'STRICT_ALL_TABLES',
-#         },
-#         'NAME': get_secret('MYSQL_DB'),
-#         'HOST': get_secret('MYSQL_HOST'),
-#         'PORT': '3306',
-#         'USER': get_secret('MYSQL_USER'),
-#         'PASSWORD': get_secret('MYSQL_PWD')
-#     }
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
 # }
 
+DATABASES = {
+     'default': {
+         'ENGINE': 'django.db.backends.mysql',
+         'OPTIONS': {
+           'sql_mode': 'STRICT_ALL_TABLES',
+         },
+         'NAME': get_secret('MYSQL_DB'),
+         'HOST': get_secret('MYSQL_HOST'),
+         'PORT': '3306',
+         'USER': get_secret('MYSQL_USER'),
+         'PASSWORD': get_secret('MYSQL_PWD')
+     }
+ }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -164,10 +169,12 @@ STATICFILES_DIRS = [
     ("css", (os.path.join(BASE_DIR,'static/css'))),
     ("images", (os.path.join(BASE_DIR,'static/images'))),
 ]
-# STATICFILES_DIRS = [(os.path.join(BASE_DIR,'static')),]
 
+#STATIC_ROOT = "/home/lar_mo/test.roadtripsandhikes.org/public/static"
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-STATIC_ROOT = (os.path.join(BASE_DIR,'static'))
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
